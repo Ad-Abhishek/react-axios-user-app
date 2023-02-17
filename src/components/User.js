86d8af4c68dcb4e1659d8b697a237485480cf2de
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2023-02-17 11:36:25 +0545

    add axios-user-app

---
+++ src/components/User.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import UserTable from "./UserTable";
+
+export const User = () => {
+  const serverUrl = process.env.REACT_APP_SERVER_URL
+  
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(serverUrl) 
+      .then((res) => {
+        console.log(res.data);
+        setUser(() => {
+          return res.data;
+        });
+      }) 
+      .catch((err) => {
+        console.log(err);
+      }); 
+  }, []);
+
+  return (
+    <>
+      <UserTable user={user} />
+    </>
+  );
+};
---
+++ src/components/UserNav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function UserNav() {
+  return (
+    <>
+      <Navbar className="bg-success">
+        <Container className="ms-2">
+          <Navbar.Brand className="text-white"><i className="bi bi-people-fill"></i> UserList</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
